0470c89a9b6058e2bcee7fd3fe33c1cedaf7c607
Caleb-Shea <shea.number.1@gmail.com>
2022-05-21 13:05:24 -0600

    Restructured colors dict

--- assets.py
+++ assets.py
@@ -9,24 +9,16 @@ for easy access.
 import pygame as pyg
 import os
 
+from helper_func import get_path
+
+
 pyg.font.init()
 pyg.mixer.pre_init(44100, -16, 2, 512)
 pyg.mixer.init()
 
-def get_path(path):
-    """
-    Description: Get the full path from a partial file path.
-    Parameters:
-        path [path-like-object] -> The partial path to get the full path from
-    Returns:
-        A path-like-object that contains a full path
-    """
-    dirname = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(dirname, path)
-
-
 fonts = {'zrnic14': pyg.font.Font(get_path(os.path.join('assets', 'fonts', 'zrnic.ttf')), 14),
          'zrnic16': pyg.font.Font(get_path(os.path.join('assets', 'fonts', 'zrnic.ttf')), 16),
+         'zrnic20': pyg.font.Font(get_path(os.path.join('assets', 'fonts', 'zrnic.ttf')), 20),
          'zrnic24': pyg.font.Font(get_path(os.path.join('assets', 'fonts', 'zrnic.ttf')), 24),
          'zrnic30': pyg.font.Font(get_path(os.path.join('assets', 'fonts', 'zrnic.ttf')), 30),
          'zrnic32': pyg.font.Font(get_path(os.path.join('assets', 'fonts', 'zrnic.ttf')), 32),
@@ -35,25 +27,26 @@ fonts = {'zrnic14': pyg.font.Font(get_path(os.path.join('assets', 'fonts', 'zrni
 
 sounds = {'ui_click': pyg.mixer.Sound(get_path(os.path.join('assets', 'sounds', 'ui_click.wav')))}
 
-colors = {'clear': (  0,   0,   0,   0),
-          'fullmenu': (  5,   0,  30,  150),
-          'white': (255, 255, 255),
-          'starwhite': (230, 230, 230),
-          'gray': (120, 120, 120),
-          'darkgray': ( 50,  50,  50),
-          'space': (  0,   0,   7),
-          'black': (  0,   0,   0),
-          'red': (255,   0,   0),
-          'orange': (220, 100,   5),
-          'yellow': (255, 255,   0),
-          'green': (  0, 255,   0),
-          'lime': (120, 255,   0),
-          'mint': (  0, 255, 170),
-          'cyan': (  0, 255, 255),
-          'skyblue': (  0, 110, 255),
-          'blue': (  0,   0, 255),
-          'deepblue': (  5,   0, 20),
-          'indigo': ( 50,   0, 255),
-          'purple': (128,   0, 255),
-          'pink': (255,   0, 255),
-          'rose': (255,   0,  80)}
+colors = {'clear': pyg.Color(  0,   0,   0,   0),
+          'fullmenu': pyg.Color(  5,   0,  30,  150),
+          'white': pyg.Color(255, 255, 255),
+          'starwhite': pyg.Color(230, 230, 230),
+          'lightgray': pyg.Color(200, 200, 200),
+          'gray': pyg.Color(120, 120, 120),
+          'darkgray': pyg.Color( 50,  50,  50), # Maybe switch to '25%white' etc
+          'space': pyg.Color(  0,   0,   7),
+          'black': pyg.Color(  0,   0,   0),
+          'red': pyg.Color(255,   0,   0),
+          'orange': pyg.Color(220, 100,   5),
+          'yellow': pyg.Color(255, 255,   0),
+          'green': pyg.Color(  0, 255,   0),
+          'lime': pyg.Color(120, 255,   0),
+          'mint': pyg.Color(  0, 255, 170),
+          'cyan': pyg.Color(  0, 255, 255),
+          'skyblue': pyg.Color(  0, 110, 255),
+          'blue': pyg.Color(  0,   0, 255),
+          'deepblue': pyg.Color(  5,   0, 20),
+          'indigo': pyg.Color( 50,   0, 255),
+          'purple': pyg.Color(128,   0, 255),
+          'pink': pyg.Color(255,   0, 255),
+          'rose': pyg.Color(255,   0,  80)}
